1cd9e248c9658ee9bfb22cda75a3dae420ee81d1
amanmakhija <amanmakhija2205@gmail.com>
2026-03-22 23:56:34 +0530

    feat: add Kafka constants for topic and consumer group names

--- src/main/java/com/aman/notification_system/constants/KafkaConstants.java
+++ src/main/java/com/aman/notification_system/constants/KafkaConstants.java
@@ -1,5 +1,10 @@
 package com.aman.notification_system.constants;
 
-public class KafkaConstants {
-    
+public final class KafkaConstants {
+
+    private KafkaConstants() {}
+
+    public static final String NOTIFICATION_TOPIC = "notification.events";
+    public static final String NOTIFICATION_GROUP  = "notification-group";
+    public static final String DLT_SUFFIX          = ".DLT";
 }
